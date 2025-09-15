1f821b8b3ec1a90f47a15db2c56acbdba6f73f1f
Sai Abhishiktha Nadakuditi <abhinadakuditi@Sais-Laptop.local>
2025-09-15 16:38:23 -0500

    modify go.mod file

--- main.go
+++ main.go
@@ -31,7 +31,6 @@ func main() {
 	log.Println("Server starting on port 8082...")
 	log.Fatal(http.ListenAndServe(":8082", nil))
 }
-
 func initKafkaProducer() (sarama.SyncProducer, error) {
 	brokerList := []string{"localhost:9092"}
 
