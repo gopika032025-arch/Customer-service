534a748544ec3f786e6dbf7516f65a9f34e23d77
gokulsai121 <gs66@njit.edu>
2025-11-19 08:42:39 -0600

    updates

--- api/routes.go
+++ api/routes.go
@@ -1,1 +1,13 @@
 package api
+
+// producer, err := sarama.NewSyncProducer([]string{"localhost:9092"}, config)
+// if err != nil {
+// 	log.Fatalf("Failed to start Kafka producer: %v", err)
+// }
+// defer producer.Close()
+
+// producer, err := sarama.NewSyncProducer([]string{"localhost:9092"}, config)
+// if err != nil {
+// 	log.Fatalf("Failed to start Kafka producer: %v", err)
+// }
+// defer producer.Close()
